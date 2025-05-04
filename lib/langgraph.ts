45b6886015bad8811bad311b3988fadf918d5a52
A8E1 <98847922+A8E1@users.noreply.github.com>
2025-05-04 14:30:26 -0500

    created the agent workflow using langgraph. did not complete the helper functions, such as shouldContinue.

--- lib/langgraph.ts
+++ lib/langgraph.ts
@@ -7,10 +7,25 @@ import {
     START,
     StateGraph
 } from "@langchain/langgraph";
+import SYSTEM_MESSAGE from "../constants/systemMessage";
+import { SystemMessage, trimMessages } from "@langchain/core/messages";
+import { ChatPromptTemplate, 
+    MessagesPlaceholder 
+} from "@langchain/core/prompts";
 
 
 //customers: https://introspection.apis.stepzen.com/customers
 //comments: https://dummyjson.com/comments
+
+//trim the messages to manage conversation history
+const trimmer = trimMessages({
+    maxTokens: 10,
+    strategy: "last", 
+    tokenCounter: (msgs) => msgs.length,
+    includeSystem: true,
+    allowPartial: false,
+    startOn: "human",
+});
 
 // Connect to wxFlows
 const toolClient = new wxflows({
@@ -40,7 +55,37 @@ const createworkflow = () => {
     const stateGraph = new StateGraph(MessagesAnnotation);.addNode(
         "agent", 
         async (state) => {
-             const systemContent = SYSTEM_MESSAGE;
+            const systemContent = SYSTEM_MESSAGE;
+
+            //Create the prompt template with system messages and messages placeholder
+
+            const promptTemplate = ChatPromptTemplate.fromMessages([
+                new SystemMessage(systemContent, {
+                    cache_control: {type: "ephemeral" },  //set a cache breakpoint (max num = 4)
+                }),
+                new MessagesPlaceholder("messages"),
+            ]);
+
+            //trim the messages to manage conversation history
+            const trimmedMessages = await trimmer.invoke(state.messages);;
+
+            //Format the prompt template with the trimmed messages
+            const formattedPrompt = await promptTemplate.invoke({ messages: trimmedMessages });
+
+            //Get the model response
+            const response = await model.invoke(formattedPrompt);
+
+            //Return the response
+            return { messages: [response] };
+            
+            
         }
-    );
+    )
+    .addEdge(START, "agent")
+    .addNode('tools', toolNode)
+    .addConditionalEdges("agent", shouldContinue)
+    .addEdge("tools", "agent")
+
+    return stateGraph;
+
 };
